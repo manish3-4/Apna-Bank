87635e1470fe955ee6c7019ceeab47234735ee9f
preeti3-4 <preetisikka553@gmail.com>
2024-11-10 18:37:49 +0530

    bank v1

--- src/data.js
+++ src/data.js
@@ -10,49 +10,6 @@ const DATA = [
         transactions: []
     },
     {
-        email: "jeff@gmail.com",
-        password: "abc123",
-        fullname: "Jeffrey de Lara",
-        type: "Savings Peso",
-        number: "47290539481",
-        balance: 1029300.43,
-        isAdmin: true, 
-        transactions: []
-    },
-    {
-        email: "jeff2@gmail.com",
-        password: "abc123",
-        fullname: "Juan dela Cruz",
-        type: "Savings Peso",
-        number: "47290539482",
-        balance: 392830.22,
-        isAdmin: false, 
-        budget: [
-            {
-                title: "Tuition fee",
-                amount: 12000
-            },
-            {
-                title: "Food take out during the pandemic",
-                amount: 4000
-            }
-        ], 
-        transactions: [
-            {
-                title: "Fund transfer", 
-                amount: 2000,
-                type: "debit", 
-                date: "October 1, 2021"
-            }, 
-            {
-                title: "Withdraw", 
-                amount: 10000, 
-                type: "debit",
-                date: "October 1, 2021"
-            }
-        ]
-    },
-    {
         email: "user24@gmail.com",
         password: "abc123",
         fullname: "Peter de Castro",
